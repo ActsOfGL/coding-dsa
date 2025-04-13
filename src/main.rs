f1a50451fbfdd1ec2ac4e3feaf8e680416d569e5
ActsOfGL <actsofgl@gmail.com>
2025-04-13 22:30:44 +0800

    feat(main): use helper and view in main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod view;
+mod helpers;
+use view::View;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! - This is ActsOfGL");
+    let view = View::new();
+    view.render_two_sum();
 }
