ec430ae9060a6cef181258ff8b9e735831e64622
ActsOfGL <actsofgl@gmail.com>
2025-04-14 19:04:13 +0800

    feat(twosum): updated variable to use borrowing instead

--- src/helpers/two_sum.rs
+++ src/helpers/two_sum.rs
@@ -22,7 +22,7 @@ mod tests {
     fn test_two_sum_found() {
         let nums = vec![2, 7, 11, 15];
         let target= 9;
-        let result = two_sum(nums, target);
+        let result = two_sum(&nums, target);
         assert_eq!(result, Some((0, 1)));
     }
 
@@ -30,7 +30,7 @@ mod tests {
     fn test_two_sum_not_found() {
         let nums = vec![1, 2, 3];
         let target= 10;
-        let result = two_sum(nums, target);
+        let result = two_sum(&nums, target);
         assert_eq!(result, None);
     }
 
@@ -38,7 +38,7 @@ mod tests {
     fn test_two_sum_duplicate_values() {
         let nums = vec![3, 3];
         let target = 6;
-        let result = two_sum(nums, target);
+        let result = two_sum(&nums, target);
         assert_eq!(result, Some((0, 1)));
     }
 }
