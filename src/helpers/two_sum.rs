bcf190609c33341f82af27321df31721a55f5e75
ActsOfGL <actsofgl@gmail.com>
2025-04-15 17:59:29 +0800

    feat(type): added types in two_sum

--- src/helpers/two_sum.rs
+++ src/helpers/two_sum.rs
@@ -1,10 +1,10 @@
 // src/helpers/two_sum.rs
 pub fn two_sum(nums: &[i32], target: i32) -> Option<(usize, usize)> {
     use std::collections::HashMap;
-    let mut map = HashMap::new();
+    let mut map: HashMap<&i32, usize>  = HashMap::new();
 
     for (i, num) in nums.iter().enumerate() {
-        let complement = target - num;
+        let complement: i32 = target - num;
         if let Some(&item) = map.get(&complement) {
             return Some((item, i));
         }
@@ -20,25 +20,25 @@ mod tests {
 
     #[test]
     fn test_two_sum_found() {
-        let nums = vec![2, 7, 11, 15];
-        let target= 9;
-        let result = two_sum(&nums, target);
+        let nums: Vec<i32> = vec![2, 7, 11, 15];
+        let target: i32= 9;
+        let result: Option<(usize, usize)> = two_sum(&nums, target);
         assert_eq!(result, Some((0, 1)));
     }
 
     #[test]
     fn test_two_sum_not_found() {
-        let nums = vec![1, 2, 3];
-        let target= 10;
-        let result = two_sum(&nums, target);
+        let nums: Vec<i32> = vec![1, 2, 3];
+        let target: i32= 10;
+        let result: Option<(usize, usize)> = two_sum(&nums, target);
         assert_eq!(result, None);
     }
 
     #[test]
     fn test_two_sum_duplicate_values() {
-        let nums = vec![3, 3];
-        let target = 6;
-        let result = two_sum(&nums, target);
+        let nums: Vec<i32> = vec![3, 3];
+        let target: i32 = 6;
+        let result: Option<(usize, usize)> = two_sum(&nums, target);
         assert_eq!(result, Some((0, 1)));
     }
 }
