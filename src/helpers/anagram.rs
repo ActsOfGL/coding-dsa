1c92c2f54f5c410451f765b584b45195de9723c6
ActsOfGL <actsofgl@gmail.com>
2025-04-15 18:58:50 +0800

    feat(anagram): fix unit test for group_anagrams

--- src/helpers/anagram.rs
+++ src/helpers/anagram.rs
@@ -16,3 +16,30 @@ pub fn group_anagrams(words: Vec<&str>) -> Vec<Vec<String>> {
 
     map.into_values().collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_group_anagrams() {
+        let words: Vec<&str> = vec!["eat", "tea", "tan", "ate", "nat", "bat"];
+        let mut expected: Vec<Vec<String>> = vec![
+            vec!["eat", "tea", "ate"],
+            vec!["tan", "nat"],
+            vec!["bat"]
+        ].into_iter()
+           .map(|group| group.into_iter().map(String::from).collect())
+           .collect();
+        let mut result: Vec<Vec<String>> = group_anagrams(words);
+        for group in &mut expected {
+            group.sort();
+        }
+        for group in &mut result {
+            group.sort();
+        }
+        expected.sort();
+        result.sort();
+        assert_eq!(expected, result);
+    }
+}
