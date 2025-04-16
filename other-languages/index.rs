e672fa9326f485b9fad2b72cb8c9926e88599fa5
ActsOfGL <actsofgl@gmail.com>
2025-04-16 22:11:34 +0800

    added TS equivalent of two_sum

--- other-languages/index.rs
+++ other-languages/index.rs
@@ -2,3 +2,21 @@
  * Other languages here
  *
  */
+
+ /**
+  * TS version
+  */
+  // two sum function in TypeScript
+  function twoSum(nums: number[], target: number): [number, number] | null {
+    const map = new Map<number, number>();
+  
+    for (let i = 0; i < nums.length; i++) {
+      const complement = target - nums[i];
+      if (map.has(complement)) {
+        return [map.get(complement)!, i];
+      }
+      map.set(nums[i], i);
+    }
+  
+    return null;
+  }
