68e208947e0d22473af19dbbfa424907fe5a1515
ActsOfGL <actsofgl@gmail.com>
2025-04-18 23:36:32 +0800

    fix(subarray): fixed code due to failing test

--- src/helpers/has_subarray_with_sum_multiple_of_k.rs
+++ src/helpers/has_subarray_with_sum_multiple_of_k.rs
@@ -4,21 +4,19 @@ pub fn has_subarray_with_sum_multiple_of_k(
   target: i32,
 ) -> bool {
     use std::collections::HashMap;
-    let mut map: HashMap<i32, usize> = HashMap::new();
+    let mut map: HashMap<i32, isize> = HashMap::new();
     let mut prefix_sum = 0;
-    map.insert(0, 0 as usize);
+    map.insert(prefix_sum, -1);
     for (i, &num) in nums.iter().enumerate() {
-        if (i == 0 && num == 0) {
-           continue;
-        }
         prefix_sum += num;
         let remainder = ((prefix_sum % target) + target) % target;
         if let Some(&prev_index) = map.get(&remainder) {
-            if (i as i32 - (prev_index as i32 + 1)) >= 2 {
+            if i as isize - prev_index >= 2 {
                 return true;
             }
+        } else {
+            map.entry(remainder).or_insert(i as isize);
         }
-        map.insert(remainder, i);
     }
     false
 }
@@ -29,7 +27,7 @@ mod tests {
 
     #[test]
     fn test_has_subarray_with_sum_multiple_of_k_true() {
-        let nums = vec![5, 3, 1, 7];
+        let nums = vec![2, 4, 3];
         let target = 6;
         assert!(has_subarray_with_sum_multiple_of_k(&nums, target));
     }
