b06ffa657ff83e3b3c18d6f827d62db1322e5826
ActsOfGL <actsofgl@gmail.com>
2025-04-19 21:43:46 +0800

    feat(mod): added next_larger_element_distances to modules

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -6,6 +6,7 @@ pub mod contains_duplicates;
 pub mod has_nearby_duplicate;
 pub mod has_nearby_divisible_duplicate;
 pub mod has_subarray_with_sum_multiple_of_k;
+pub mod next_larger_element_distances;
 
 // add other helper functions here
 pub use two_sum::two_sum;
@@ -14,3 +15,4 @@ pub use contains_duplicates::contains_duplicates;
 pub use has_nearby_duplicate::has_nearby_duplicate;
 pub use has_nearby_divisible_duplicate::has_nearby_divisible_duplicate;
 pub use has_subarray_with_sum_multiple_of_k::has_subarray_with_sum_multiple_of_k;
+pub use next_larger_element_distances::next_larger_element_distances;
