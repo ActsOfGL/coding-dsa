82a98a4e305894ec53d06d93b30c2010322878f6
ActsOfGL <actsofgl@gmail.com>
2025-04-20 20:07:09 +0800

    feat(helpers): updated max_sum_sliding_window

--- src/helpers/max_sum_sliding_window.rs
+++ src/helpers/max_sum_sliding_window.rs
@@ -7,18 +7,29 @@ pub fn max_sum_sliding_window(
         return 0;
     }
 
-    let mut current_sum = 0;
-    for i in 0..k {
-        current_sum += nums[i]; // Sum first window
-    }
+    let mut current_sum: i32 = nums.iter().take(k).sum();
+    /*
+      * code equivalent for nums.iter().take(k).sum();
+      * the one above uses Rust's built-in max function and idiomatic syntax
+      * let mut current_sum = 0;
+      * for i in 0..k {
+      *     current_sum += nums[i];
+      * }
+      */
 
     let mut max_sum = current_sum;
 
     for i in k..nums.len() {
-        current_sum = current_sum + nums[i] - nums[i - k]; // Slide window
-        if current_sum > max_sum {
-            max_sum = current_sum;
-        }
+        current_sum = current_sum + nums[i] - nums[i - k];
+
+        max_sum = max_sum.max(current_sum);
+        /*
+         * code equivalent for max_sum.max(current_sum)
+         * the one above uses Rust's built-in max function and idiomatic syntax
+         * if current_sum > max_sum {
+         *     max_sum = current_sum;
+         * }
+        */
     }
 
     max_sum
