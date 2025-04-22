637518bfff7fb8a56c19bf9fdfefc7d0a60e320c
ActsOfGL <actsofgl@gmail.com>
2025-04-22 15:32:24 +0800

    feat(mod): added max_sum_with_prefix_sum

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -8,6 +8,7 @@ pub mod has_nearby_divisible_duplicate;
 pub mod has_subarray_with_sum_multiple_of_k;
 pub mod next_larger_element_distances;
 pub mod max_sum_sliding_window;
+pub mod max_sum_with_prefix_sum;
 
 // add other helper functions here
 pub use two_sum::two_sum;
@@ -18,3 +19,4 @@ pub use has_nearby_divisible_duplicate::has_nearby_divisible_duplicate;
 pub use has_subarray_with_sum_multiple_of_k::has_subarray_with_sum_multiple_of_k;
 pub use next_larger_element_distances::next_larger_element_distances;
 pub use max_sum_sliding_window::max_sum_sliding_window;
+pub use max_sum_with_prefix_sum::max_sum_with_prefix_sum;
