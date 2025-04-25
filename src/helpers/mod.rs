d59473af23861c2725c77f00271b7f191cc7fa6e
ActsOfGL <actsofgl@gmail.com>
2025-04-25 18:24:33 +0800

    feat(mod): added get_length_of_shortest_window in mod

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -9,6 +9,7 @@ pub mod has_subarray_with_sum_multiple_of_k;
 pub mod next_larger_element_distances;
 pub mod max_sum_sliding_window;
 pub mod max_sum_with_prefix_sum;
+pub mod get_length_of_shortest_window;
 
 // add other helper functions here
 pub use two_sum::two_sum;
@@ -20,3 +21,4 @@ pub use has_subarray_with_sum_multiple_of_k::has_subarray_with_sum_multiple_of_k
 pub use next_larger_element_distances::next_larger_element_distances;
 pub use max_sum_sliding_window::max_sum_sliding_window;
 pub use max_sum_with_prefix_sum::max_sum_with_prefix_sum;
+pub use get_length_of_shortest_window::get_length_of_shortest_window;
