d05eba84d6d0263de9e37752118b1695b7c48446
ActsOfGL <actsofgl@gmail.com>
2025-04-27 16:34:45 +0800

    chore(helper): added type in unit test for get_max_stable_session_count

--- src/helpers/get_max_stable_session_count.rs
+++ src/helpers/get_max_stable_session_count.rs
@@ -23,15 +23,21 @@ mod tests {
 
     #[test]
     fn test_get_max_stable_session_count() {
-        let logs = vec![20, 10, 5, 25, 15, 5];
-        let threshold = 40;
-        assert_eq!(get_max_stable_session_count(&logs, threshold), 3);
+        let logs: Vec<i32> = vec![20, 10, 5, 25, 15, 5];
+        let threshold: i32 = 40;
+        assert_eq!(
+            get_max_stable_session_count(
+                &logs,
+                threshold
+            ),
+            3
+        );
     }
 
     #[test]
     fn test_get_max_stable_session_count_empty() {
-        let logs = Vec::new();
-        let threshold = 40;
+        let logs: Vec<i32> = Vec::new();
+        let threshold: i32 = 40;
         assert_eq!(
           get_max_stable_session_count(
             &logs,
