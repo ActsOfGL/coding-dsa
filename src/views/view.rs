36a305a8faa0330a3f0879c5b9399d8224d369d7
ActsOfGL <actsofgl@gmail.com>
2025-04-29 14:56:21 +0800

    chore(view): added types in view

--- src/views/view.rs
+++ src/views/view.rs
@@ -91,7 +91,7 @@ impl View {
 
     pub fn render_max_sum_sliding_window(&self) {
         let nums: Vec<i32> = vec![1, 2, 1, 5, 1, 3, 1];
-        let k = 3;
+        let k: usize = 3;
         println!(
             "Max sum of subarray within an array using sliding window: {}",
             max_sum_sliding_window(&nums, k)
@@ -100,7 +100,7 @@ impl View {
 
     pub fn render_max_sum_sliding_window_of_k_adaptive(&self) {
         let nums: Vec<i32> = vec![1, 2, 1, 5, 1, 3, 1];
-        let k = 3;
+        let k: usize = 3;
         println!(
             "Max sum of subarray within an array using sliding window of k (adaptive): {}",
             max_sum_sliding_window_of_k_adaptive(&nums, k)
@@ -109,7 +109,7 @@ impl View {
 
     pub fn render_max_sum_with_prefix_sum(&self) {
         let nums: Vec<i32> = vec![1, 2, 1, 5, 1, 3, 1];
-        let k = 3;
+        let k: usize = 3;
         println!(
             "Max sum of subarray within an array using prefix sum: {}",
             max_sum_with_prefix_sum(&nums, k)
