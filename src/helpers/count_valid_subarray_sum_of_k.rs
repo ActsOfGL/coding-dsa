5bb5b3f6e03597ba7f221b1f4b450db36a17268a
ActsOfGL <actsofgl@gmail.com>
2025-05-03 19:56:50 +0800

    feat(helpers): added typings and fixed the value in count_valid_subarray_sum_of_k

--- src/helpers/count_valid_subarray_sum_of_k.rs
+++ src/helpers/count_valid_subarray_sum_of_k.rs
@@ -8,18 +8,20 @@ pub fn count_valid_subarray_sum_of_k(
     min_len: i32,
 ) -> i32 {
     
-    let mut map = HashMap::new();
-    let mut sum = 0;
-    let mut count = 0;
+    let mut map: HashMap<i32, Vec<isize>> = HashMap::new();
+    let mut sum: i32 = 0;
+    let mut count: i32 = 0;
     
-    map.entry(0).or_insert(vec![-1usize]); // handle subarrays starting at index 0
+    map.entry(0).or_insert(vec![-1]); // handle subarrays starting at index 0
     
     for (i, &log) in logs.iter().enumerate() {
+        let i: isize = i as isize; // from usize to isize
         sum += log;
         
         if let Some(indices) = map.get(&(sum - k)) {
             for &j in indices {
-                if i - j >= (min_len - 1) as usize {
+                // (i -j + 1) >= min_len as isize or
+                if i - j >= min_len as isize - 1 {
                     count += 1;
                 }
             }
