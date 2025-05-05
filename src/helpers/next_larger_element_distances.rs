937ec0cdbbae6062043e62e54caae93d0c8291cd
ActsOfGL <actsofgl@gmail.com>
2025-05-05 19:37:01 +0800

    chore(helpers): added types in next_larger_element_distances

--- src/helpers/next_larger_element_distances.rs
+++ src/helpers/next_larger_element_distances.rs
@@ -1,6 +1,6 @@
 // src/helpers/next_larger_element_distances.rs
 pub fn next_larger_element_distances(nums: &[i32]) -> Vec<i32> {
-    let mut res = vec![0; nums.len()];
+    let mut res: Vec<i32> = vec![0; nums.len()];
     let mut stack: Vec<usize> = Vec::new();
 
     for i in 0..nums.len() {
