55fdfa8947e4faf7f6f137b18ed01b2f9b2d8967
ActsOfGL <actsofgl@gmail.com>
2025-05-18 18:20:03 +0800

    chore(helpers): fixed lint in test_max_sum_sliding_window_of_k_adaptive_zero

--- src/helpers/max_sum_sliding_window.rs
+++ src/helpers/max_sum_sliding_window.rs
@@ -117,8 +117,11 @@ mod tests {
         let logs: Vec<i32> = vec![5, 1, 3, 7, 2, 6, 4];
         let k: usize = 0;
         assert_eq!(
-            max_sum_sliding_window_of_k_adaptive(&logs, k),
-            0
+            max_sum_sliding_window_of_k_adaptive(
+                &logs,
+                k,
+            ),
+            0,
         ); // subarray [3,7,2] -> sum = 12, [7,2,6] -> sum = 15
     }
     
