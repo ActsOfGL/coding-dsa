f97e14f90fedccc24e2f2c87abd074c9163771be
ActsOfGL <actsofgl@gmail.com>
2025-05-19 21:17:43 +0800

    feat(helpers): added get_subarray_length_bound_diff_at_most_k

--- src/helpers/count_subarrays_with_bounded_diff.rs
+++ src/helpers/count_subarrays_with_bounded_diff.rs
@@ -50,6 +50,55 @@ pub fn count_subarrays_with_bounded_diff(
 
 }
 
+/**
+ * @returns: the length of the longest subarray with a maximum difference of `k` at most
+ */
+fn get_subarray_length_bound_diff_at_most_k(nums: &[i32], k: i32) -> usize {
+    let mut max_deque= VecDeque::new();
+    let mut min_deque= VecDeque::new();
+    let mut left = 0;
+    let mut max_len = 0;
+
+    for (right, &num) in nums.iter().enumerate() {
+        // Maintain decreasing order for max_deque
+        while let Some(&last) = max_deque.back() {
+            if nums[last] <= num {
+                max_deque.pop_back();
+            } else {
+                break;
+            }
+        }
+        max_deque.push_back(right);
+
+        // Maintain increasing order for min_deque
+        while let Some(&last) = min_deque.back() {
+            if nums[last] >= num {
+                min_deque.pop_back();
+            } else {
+                break;
+            }
+        }
+        min_deque.push_back(right);
+
+        // Shrink window if the current max - min exceeds k
+        while nums[*max_deque.front().unwrap()] - nums[*min_deque.front().unwrap()] > k {
+            left += 1;
+            // Remove out-of-bound indices from the deques
+            if *max_deque.front().unwrap() < left {
+                max_deque.pop_front();
+            }
+            if *min_deque.front().unwrap() < left {
+                min_deque.pop_front();
+            }
+        }
+
+        // Update max_len with the current valid window size
+        max_len = max_len.max(right - left + 1);
+    }
+
+    max_len
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
