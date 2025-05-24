19b7201cca8d58ee9ad7367134473dfcdcdd0c4e
ActsOfGL <actsofgl@gmail.com>
2025-05-25 00:36:34 +0800

    chore(helpers): added type in test_has_nearby_duplicate_distance_zero

--- src/helpers/has_nearby_duplicate.rs
+++ src/helpers/has_nearby_duplicate.rs
@@ -74,7 +74,7 @@ mod tests {
     #[test]
     fn test_has_nearby_duplicate_distance_zero() {
         let nums: Vec<i32> = vec![1, 2, 3, 1];
-        let distance = 0;
+        let distance: i32 = 0;
         assert_eq!(
             has_nearby_duplicate(&nums, distance),
             false,
