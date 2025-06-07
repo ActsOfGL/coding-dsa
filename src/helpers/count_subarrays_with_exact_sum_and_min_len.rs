bb6b75a4650862dc2e9a2bc04237694a01485186
ActsOfGL <actsofgl@gmail.com>
2025-06-07 19:32:30 +0800

    chore(helpers): added type for min_len in test_count_subarrays_with_exact_sum_and_min_len_empty_array

--- src/helpers/count_subarrays_with_exact_sum_and_min_len.rs
+++ src/helpers/count_subarrays_with_exact_sum_and_min_len.rs
@@ -64,7 +64,7 @@ mod tests {
     fn test_count_subarrays_with_exact_sum_and_min_len_empty_array() {
         let nums: Vec<i32> = vec![];
         let k: i32 = 2;
-        let min_len = 2;
+        let min_len: usize = 2;
         assert_eq!(count_subarrays_with_exact_sum_and_min_len(&nums,k,min_len), 0);
     }
 }
