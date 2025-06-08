bd2e6b48d360c83e5544900006ac0f86565c92e1
ActsOfGL <actsofgl@gmail.com>
2025-06-08 20:21:26 +0800

    chore(helpers): fixed linting space in test_count_subarrays_with_bounded_sum_basic

--- src/helpers/count_subarrays_with_bounded_sum.rs
+++ src/helpers/count_subarrays_with_bounded_sum.rs
@@ -36,6 +36,13 @@ mod tests {
         let nums: Vec<i32> = vec![2, 1, 4, 5, 1];
         let k: i32 = 5;
         let min_len: usize = 2;
-        assert_eq!(count_subarrays_with_bounded_sum(&nums, k, min_len), 3);
+        assert_eq!(
+            count_subarrays_with_bounded_sum(
+                &nums,
+                k,
+                min_len,
+            ),
+            3,
+        );
     }
 }
