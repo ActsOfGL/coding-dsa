252baf2b45e846d04043f230c021fd5d9ce0c270
ActsOfGL <actsofgl@gmail.com>
2025-06-10 13:52:26 +0800

    chore(helpers): fixed linting space in test_has_nearby_duplicate_false

--- src/helpers/has_nearby_duplicate.rs
+++ src/helpers/has_nearby_duplicate.rs
@@ -39,7 +39,10 @@ mod tests {
         let nums: Vec<i32> = vec![1, 2, 3, 1, 2, 3];
         let distance: i32 = 2;
         assert_eq!(
-            has_nearby_duplicate(&nums, distance),
+            has_nearby_duplicate(
+                &nums,
+                distance,
+            ),
             false,
         );
     }
