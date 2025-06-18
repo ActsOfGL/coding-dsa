60d35e2861e2c0d11563c3c29a7abeba615063dc
ActsOfGL <actsofgl@gmail.com>
2025-06-18 18:22:43 +0800

    refactor(helpers): added expected variable in test_two_sum_duplicate_values

--- src/helpers/two_sum.rs
+++ src/helpers/two_sum.rs
@@ -52,6 +52,7 @@ mod tests {
         let nums: Vec<i32> = vec![3, 3];
         let target: i32 = 6;
         let result: Option<(usize, usize)> = two_sum(&nums, target);
+        let expected = Some((0, 1));
         assert_eq!(result, Some((0, 1)));
     }
 }
