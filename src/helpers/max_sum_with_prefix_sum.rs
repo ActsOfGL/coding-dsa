2ddb0b3e900dfb8d69bf5af6b3cb36a7e5b39790
ActsOfGL <actsofgl@gmail.com>
2025-06-23 18:37:20 +0800

    refactor(test): added expected in test_max_sum_with_prefix_sum

--- src/helpers/max_sum_with_prefix_sum.rs
+++ src/helpers/max_sum_with_prefix_sum.rs
@@ -29,6 +29,7 @@ mod tests {
         let nums: Vec<i32> = vec![1, 2, 1, 5, 1, 3, 1];
         let k: usize = 3;
         let output = max_sum_with_prefix_sum(&nums, k);
+        let expected = 9;
         assert_eq!(
             max_sum_with_prefix_sum(&nums, k),
             9,
