cee3c802b7e1f86018d38217cd52b600ecaa3032
ActsOfGL <actsofgl@gmail.com>
2025-08-07 20:43:11 +0800

    refactor(helpers): added variable output in test_max_sum_sliding_window

--- src/helpers/max_sum_sliding_window.rs
+++ src/helpers/max_sum_sliding_window.rs
@@ -64,6 +64,7 @@ mod tests {
     fn test_max_sum_sliding_window() {
         let nums: Vec<i32> = vec![1, 2, 1, 5, 1, 3, 1];
         let k: usize = 3;
+        let output = max_sum_sliding_window(&nums, k);
         assert_eq!(
             max_sum_sliding_window(
                 &nums,
